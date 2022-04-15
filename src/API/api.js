abca486cbb585a4cc61565731ecd22121d62be18
nggoong <nggoong@naver.com>
2022-04-15 21:19:12 +0900

    api를 이용하는 axios 함수 구현✔

--- src/API/api.js
+++ src/API/api.js
@@ -15,4 +15,14 @@ const clothesPostPosting = (PostingInput) => {
     return axios.post(url, PostingInput);
 }
 
-export {signUp, login, clothesPostPosting};
+const codiPostPosting = (PostingInput) => {
+    let url = '/api/posting/codi-post';
+    return axios.post(url, PostingInput);
+}
+
+const getClothesPost = (num) => {
+    let url = `/api/posting/clothes-post/count/${num}`;
+    return axios.get(url);
+}
+
+export {signUp, login, clothesPostPosting, getClothesPost};
